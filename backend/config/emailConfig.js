c7bfe30206b48f7c876fdc39c5e23b277f77c060
MohitRathi14 <aaditya375singh2@gmail.com>
2025-12-17 10:54:07 +0530

    change the name of the EMAIL_PASS TO EMAIL_PASSWORD

--- backend/config/emailConfig.js
+++ backend/config/emailConfig.js
@@ -4,6 +4,6 @@ export const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.EMAIL_USER, // your gmail
-        pass: process.env.EMAIL_PASS, // your app password
+        pass: process.env.EMAIL_PASSWORD, // your app password
     },
 });
